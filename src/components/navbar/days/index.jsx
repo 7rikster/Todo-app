6bb7d5ef706903013483866607a0ae034c1e43a1
Priyanshu <ribupkashyap@gmail.com>
2024-10-16 13:53:57 +0530

    added active effect on nav items

--- src/components/navbar/days/index.jsx
+++ src/components/navbar/days/index.jsx
@@ -2,20 +2,24 @@ import { PiCalendarBold } from "react-icons/pi";
 import { IoCalendarNumberOutline } from "react-icons/io5";
 import { FaCalendarCheck } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
-import "./style.css"
+import { useNavigate, useResolvedPath, useMatch, useParams } from "react-router-dom";
+import "./style.css";
 
 function DaysCard() {
 
     const navigate = useNavigate();
-
+    function addClass( link ){
+        const resolvedPath = useResolvedPath(link);
+        const isActive = useMatch({ path: resolvedPath.pathname, end: true});
+        return isActive? "nav-item active" : "nav-item";
+    }
 
     return ( 
         <div className="container">
-            <div className="nav-item" onClick={() => navigate('/')}> <i><FaCalendarCheck /></i> Today</div>
-            <div className="nav-item" onClick={() => navigate('/tomorrow')}> <i><FaCalendar /></i>Tomorrow</div>
-            <div className="nav-item" onClick={() => navigate('/this-month')}> <i><IoCalendarNumberOutline /></i>This Month</div>
-            <div className="nav-item" onClick={() => navigate('/next-month')}> <i><IoCalendarNumberOutline /></i>Next Month</div>
+            <div className={addClass('/')} onClick={() => navigate('/')}> <i><FaCalendarCheck /></i> Today</div>
+            <div className={addClass('tomorrow')} onClick={() => navigate('/tomorrow')}> <i><FaCalendar /></i>Tomorrow</div>
+            <div className={addClass('this-month')} onClick={() => navigate('/this-month')}> <i><IoCalendarNumberOutline /></i>This Month</div>
+            <div className={addClass('next-month')} onClick={() => navigate('/next-month')}> <i><IoCalendarNumberOutline /></i>Next Month</div>
         </div>
     );
 }
--- src/components/navbar/state-of-task/index.jsx
+++ src/components/navbar/state-of-task/index.jsx
@@ -2,18 +2,22 @@ import { FaCalendarXmark } from "react-icons/fa6";
 import { FaCalendarMinus } from "react-icons/fa6";
 import { FaCalendarCheck } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useResolvedPath, useMatch } from "react-router-dom";
 import "./style.css"
 
 function StateOfTasks() {
 
     const navigate = useNavigate();
-
+    function addClass( link ){
+        const resolvedPath = useResolvedPath(link);
+        const isActive = useMatch({ path: resolvedPath.pathname, end: true});
+        return isActive? "nav-item active" : "nav-item";
+    }
     return ( 
         <div className="states-container">
-            <div className="nav-item" onClick={() => navigate('/pending')}> <i><FaCalendarMinus /></i>Pending Tasks</div>
-            <div className="nav-item" onClick={() => navigate('/completed')}> <i><FaCalendarCheck /></i>Completed Tasks</div>
-            <div className="nav-item" onClick={() => navigate('/incompleted')}> <i><FaCalendarXmark /></i>Incompleted Tasks</div>
+            <div className={addClass('pending')} onClick={() => navigate('/pending')}> <i><FaCalendarMinus /></i>Pending Tasks</div>
+            <div className={addClass('completed')} onClick={() => navigate('/completed')}> <i><FaCalendarCheck /></i>Completed Tasks</div>
+            <div className={addClass('incompleted')} onClick={() => navigate('/incompleted')}> <i><FaCalendarXmark /></i>Incompleted Tasks</div>
         </div>
     );
 }
